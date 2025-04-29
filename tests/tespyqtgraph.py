56cc52fcb1c970fbcfdbf33f9b1be505445cef27
Quan Qing <qingbsd@gmail.com>
2025-04-29 07:20:13 -0700

    added a couple of tests

---
+++ tests/tespyqtgraph.py
@@ -0,0 +1,12 @@
+import pyqtgraph as pg
+import numpy as np
+x = np.arange(1000)
+y = np.random.normal(size=(3, 1000))
+plotWidget = pg.plot(title="Three plot curves")
+for i in range(3):
+    plotWidget.plot(x, y[i], pen=(i,3))  ## setting pen=(i,3) automaticaly creates three different-colored pens
+    
+    
+x = np.random.normal(size=1000)
+y = np.random.normal(size=1000)
+pg.plot(x, y, pen=None, symbol='o')  ## setting pen=None disables line drawing
